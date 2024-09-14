e681a72709e55672bdcbf6a972535196dbac2aab
Orkhand4 <orkhand4@mail.ru>
2024-09-14 10:21:08 +0400

    Promise

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-console.log('HAKUNA');
+const demo = () => {
+  return new Promise((resolve, reject)=> setTimeout(resolve,5000));
+};
 
-setTimeout(() => {
-  console.log('MATATA');
-}, 5000);
+const memo = async () => {
+  console.log('hakuna');
+  await demo();
+  console.log('matata');
+  console.log('huba muba');
+};
 
-console.log('HUBA MUBA');
+memo()
 
