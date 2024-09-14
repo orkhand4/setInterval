8a49b0c5283c2c5ccd4eea6bd4f0b1eda6332ace
Orkhand4 <orkhand4@mail.ru>
2024-09-14 11:46:28 +0400

    DOM

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const h1_tag = document.getElementById('demo')
-
+const h1_tag1 = document.querySelectorAll('h1')
+const h1_tag2 = document.getElementById('h1')
+const h1_tag3 = document.getElementsByClassName('h1')
+const h1_tag = document.getElementsByTagName('h1')
 console.log(h1_tag);
