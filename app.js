afa416a38e63ca9275e11f0e13353a3c21987ef5
Orkhand4 <orkhand4@mail.ru>
2024-09-14 11:37:41 +0400

    dom

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-class Person {
-  constructor(name, surname, age, location) {
-    this.name = name;
-    this.surname = surname;
-    this.age = age;
-    this.location -location;
-  }
-}
+const h1_tag = document.getElementById('demo')
 
-class Director extends Person {
-  constructor(name, surname, age, location, specialty, salary) {
-    super(name, surname, age, location);
-    this.specialty = specialty;
-    this.salary = salary;
-  }
-}
-
-const director1 = new Director('Zahid', 'Vahabzade', 12, 'Baki', 'Devops', 1200)
-console.log(director1);
-
+console.log(h1_tag);
